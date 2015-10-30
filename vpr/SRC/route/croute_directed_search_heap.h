b5b88ecd53a9adadcbb06629cc4b4f233928191e
Elias Vansteenkiste <Elias.Vansteenkiste@ugent.be>
2015-10-30 02:04:49 +0100

    removing old vtr_flow files

--- vpr/SRC/route/croute_directed_search_heap.h
+++
@@ -1,2 +0,0 @@
-boolean try_heap_conr_ds_route(struct s_router_opts router_opts,
-                          t_ivec ** clb_opins_used_locally);
--- vpr/SRC/route/croute_timing.h
+++
@@ -1,5 +0,0 @@
-boolean try_timing_driven_route_conr(struct s_router_opts router_opts,
-        float **net_delay,
-        t_slack * slacks,
-        t_ivec ** clb_opins_used_locally,
-        boolean timing_analysis_enabled);
